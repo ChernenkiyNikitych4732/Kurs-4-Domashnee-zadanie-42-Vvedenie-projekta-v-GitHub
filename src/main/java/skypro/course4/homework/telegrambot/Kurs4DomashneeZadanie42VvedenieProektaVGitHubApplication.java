4173159737ae060efc252cab1a54bc826b3e8960
chern <chernenkij.nikitych@gmail.com>
2023-12-27 11:41:16 +0100

    Курс 4 Домашнее задание 42 Введение проекта в GitHub - изначальный комментарий

---
+++ src/main/java/skypro/course4/homework/telegrambot/Kurs4DomashneeZadanie42VvedenieProektaVGitHubApplication.java
@@ -0,0 +1,13 @@
+package skypro.course4.homework.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs4DomashneeZadanie42VvedenieProektaVGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurs4DomashneeZadanie42VvedenieProektaVGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/course4/homework/telegrambot/Kurs4DomashneeZadanie42VvedenieProektaVGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.course4.homework.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurs4DomashneeZadanie42VvedenieProektaVGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
